ce08c5375b6be4530b489680376dc2799fb5643f
Thusi Selvaratnam <s_thusi@hotmail.com>
2022-01-21 00:21:51 +0100

    fixed

--- lunchbox.react/src/App.js
+++ lunchbox.react/src/App.js
@@ -1,12 +1,16 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="grid-container">
-      <header>
-      </header>
-      <header>
-      </header>
-      <main>
-      </main>
+        <header-container>
+          <Header />
+        </header-container>
+        <navigation-container>Navigation</navigation-container>
+        <main-container>
+          <div class="main">Main</div>
+          <div class="cart">fsfsf sfsfsf sf fsfsf sf sfsf sf sfs sfsf sfs fsf sf sf sf sdfsdfs sdf sf sfsf sdf sdf sf sfs fsdf sdf sf sfsd fsdf sdf sdffsdfsdf sf sdf sfsdf sdf sdf</div>
+        </main-container>
     </div>
   );
 }
---
+++ lunchbox.react/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header-wrapper">
+                <div className="back">Back</div>
+                <div className="logo">
+                    <img src="http://www.lunchbox.dk/assets/images/logo/white.svg" onclick="link_navigation('index');" class="default-icon" alt="" border="0"></img>
+                </div>
+                <div className="memebership"><i className="fa fa-user"></i><a href="#">Login som medlem</a></div>
+            </div>
+        )
+    }
+}
