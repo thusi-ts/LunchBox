36cc74620672aeecad873ff7964ef8be598c2241
Thusi Selvaratnam <s_thusi@hotmail.com>
2022-02-02 16:00:47 +0100

    css

--- lunchbox.react/src/components/pages/Info.js
+++ lunchbox.react/src/components/pages/Info.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export default class Info extends Component {
+
+    constructor(props){
+        
+        super(props);
+        
+    }
+
     render() {
+
+        const tabs = 
+            <Tabs>
+              <TabList>
+                <Tab>Mario</Tab>
+                <Tab disabled>Luigi</Tab>
+                <Tab>Peach</Tab>
+              </TabList>
+          
+              <TabPanel>
+                <p>
+                  hello
+                </p>
+                <p>
+                  Source:{' '}
+                  
+                </p>
+              </TabPanel>
+              <TabPanel>
+                <p>
+                  test1
+                </p>
+                <p>
+                  Source:{' '}
+                  
+                </p>
+              </TabPanel>
+              <TabPanel>
+                <p>
+                  test
+                </p>
+                <p>
+                  Source:{' '}
+                  
+                </p>
+              </TabPanel>
+             
+            </Tabs>
+
+
         return (
             <div>
-                Info
+                { tabs }
             </div>
         )
     }
